347b428c638b2369a418a762e2e7ef9b1d97a869
cmoulini <mouliniercaro@gmail.com>
2021-02-05 11:22:09 +0100

    Started Stop command

---
+++ src/shell/command/stop.rs
@@ -0,0 +1,14 @@
+use crate::shell::command::utils::Command;
+use crate::Config;
+use std::error::Error;
+use std::str::SplitAsciiWhitespace;
+
+const STOP_USAGE: &'static str =
+"stop <name>\t\tStop a process\nstop <name> <name>\t\tStop multiple processes\nstop all\t\tStop all processes";
+
+pub struct Stop<'a> {
+	args: SplitAsciiWhitespace<'a>,
+	config: &'a mut Config,
+	process_names: Option<Vec<&'a str>>,
+	all_option: bool,
+}
