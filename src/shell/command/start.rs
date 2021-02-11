83f04ca78fcef17fa896a3e78270ee3558e0f6ea
Jeremie <jeremiegthr@gmail.com>
2021-02-11 15:31:18 +0100

    remove a \t foor nice formatting

--- src/shell/command/start.rs
+++ src/shell/command/start.rs
@@ -4,7 +4,7 @@ use std::error::Error;
 use std::str::SplitAsciiWhitespace;
 
 const START_USAGE: &'static str =
-	"start <name>\t\tStart a process\nstart <name> <name>\t\tStart multiple processes\nstart all\t\tStart all processes";
+	"start <name>\t\tStart a process\nstart <name> <name>\tStart multiple processes\nstart all\t\tStart all processes";
 
 pub struct Start<'a> {
 	args: SplitAsciiWhitespace<'a>,
