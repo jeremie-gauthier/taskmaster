ee9ca0cb72f8f022f235462ea8c70c8daf924e8a
Jeremie <jeremiegthr@gmail.com>
2021-06-01 21:02:02 +0200

    add listener class

---
+++ src/lib/tcp/TCPListener.class.ts
@@ -0,0 +1,39 @@
+import TCPMessage from "./TCPMessage.class.ts";
+
+type TCPListenerOptions = {
+  maxConn: number;
+};
+
+type OnConnectCallback = (TCPMsg: TCPMessage) => Promise<void>;
+
+export default class TCPListener {
+  private activeConn = 0;
+  private options = {
+    maxConn: 5,
+  };
+  private listener: Deno.Listener;
+
+  constructor(port: number, options?: Partial<TCPListenerOptions>) {
+    this.options = { ...this.options, ...options };
+    this.listener = Deno.listen({ port });
+    console.log("Listening on 0.0.0.0:8080");
+  }
+
+  private canConnect = () => this.activeConn < this.options.maxConn;
+
+  async handleIncomingConn(onConnect: OnConnectCallback) {
+    for await (const conn of this.listener) {
+      const TCPMsg = new TCPMessage(conn);
+
+      // using `then` instead of `async` here for non-blocking thread
+      // else it would stuck and block other connections
+      if (this.canConnect()) {
+        onConnect(TCPMsg)
+          .then(conn.close);
+      } else {
+        TCPMsg.write("[-] Cannot connect to taskmaster daemon (No space left)")
+          .then(conn.close);
+      }
+    }
+  }
+}
