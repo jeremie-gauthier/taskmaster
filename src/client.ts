f6d20e617e9a0d323b03982700764749e0dcffaf
Jeremie <jeremiegthr@gmail.com>
2021-06-10 20:41:11 +0200

    use config var for TCP PORT

--- src/client.ts
+++ src/client.ts
@@ -1,9 +1,10 @@
 import repl from "./lib/repl/index.ts";
 import TCPConnecter from "./lib/tcp/TCPConnecter.class.ts";
+import { TCP_PORT } from "./config.ts";
 
 (async () => {
   try {
-    const tcp = new TCPConnecter(8080);
+    const tcp = new TCPConnecter(TCP_PORT);
     await tcp.Ready;
 
     if (tcp.TCPMsg) {
