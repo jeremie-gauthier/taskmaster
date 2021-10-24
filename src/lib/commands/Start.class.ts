91df9e001868f8576c9e1a078c095a58d61f070d
Jeremie <jeremie@allisone.ai>
2021-10-24 18:13:38 +0200

    fix: cyclic deps

--- src/lib/commands/Start.class.ts
+++ src/lib/commands/Start.class.ts
@@ -1,5 +1,5 @@
 import Command from "./Command.class.ts";
-import Container from "../process/Container.class.ts";
+import Processes from "../process/Container.class.ts";
 import { isEmpty } from "../utils/index.ts";
 
 export default class Start extends Command {
@@ -12,22 +12,17 @@ export default class Start extends Command {
       return this.usage();
     }
 
-    const Processes = Container.getInstance().processes;
+    const processes = Processes.getInstance().processes;
     const processResponses: string[] = [];
 
     for (const arg of this.args) {
-      const currentProcess = Processes[arg];
-      if (!currentProcess) {
+      const currentProcess = processes[arg];
+
+      if (currentProcess) {
+        processResponses.push(currentProcess.start());
+      } else {
         processResponses.push(`${arg}: not found`);
       }
-
-      // format args (from config file) for Deno
-      const cmd = currentProcess.config.cmd?.split(/\s+/) ?? [];
-      const handle = Deno.run({ cmd });
-
-      // bind the subprocess handler for later calls
-      currentProcess.handle = handle;
-      processResponses.push(`${arg}: started`);
     }
     return processResponses.join("\n");
   }
--- src/lib/process/Process.class.ts
+++ src/lib/process/Process.class.ts
@@ -6,11 +6,15 @@ export default class Process {
   private _config: ProcessConfig;
   private _status: ProcessStatus = "STOPPED";
   private _startRetries = 0;
+  private _lastTimeEvent: Date | null = null;
   private _handle: Deno.Process<Deno.RunOptions> | null = null;
 
   constructor(name: string, config: ProcessConfig) {
     this.name = name;
     this._config = config;
+    if (config.autoStart) {
+      this.start();
+    }
   }
 
   get config() {
@@ -21,6 +25,10 @@ export default class Process {
     return this._status;
   }
 
+  get lastTimeEvent() {
+    return this._lastTimeEvent;
+  }
+
   get handle() {
     return this._handle;
   }
@@ -29,14 +37,30 @@ export default class Process {
     this._handle = handle;
 
     if (handle) {
+      this._status = "RUNNING";
+      this._lastTimeEvent = new Date();
       handle.status().then(({ success, code, signal }) => {
         if (success) {
           this._status = "EXITED";
+          this._lastTimeEvent = new Date();
           this._handle = null;
         }
       });
     } else {
+      this._lastTimeEvent = new Date();
       this._status = "STOPPED";
     }
+  }
+
+  start() {
+    if (this._status === "RUNNING") {
+      return `${this.name}: ERROR (already started)`;
+    }
+
+    // format args (from config file) for Deno
+    const cmd = this._config.cmd?.split(/\s+/) ?? [];
+    this.handle = Deno.run({ cmd });
+
+    return `${this.name}: started`;
   }
 }
