8c7b5d3ad9d566ed81e93188705067827b366e6a
Jeremie <jeremie@allisone.ai>
2021-10-30 07:52:54 +0200

    feat: support restart cmd

--- src/lib/commands/Restart.class.ts
+++ src/lib/commands/Restart.class.ts
@@ -1,3 +1,5 @@
+import Container from "../process/Container.class.ts";
+import { isEmpty } from "../utils/index.ts";
 import Command from "./Command.class.ts";
 
 export default class Restart extends Command {
@@ -5,8 +7,22 @@ export default class Restart extends Command {
     super(args);
   }
 
-  exec() {
-    return "restart";
+  async exec() {
+    if (isEmpty(this.args)) {
+      return this.usage();
+    }
+
+    const allResponses: string[] = [];
+    for (const arg of this.args) {
+      const currentProcess = Container.getInstance().getProcess(arg);
+
+      const stopRes = await currentProcess.stop();
+      allResponses.push(stopRes);
+
+      const startRes = await currentProcess.start({ commandFromUser: true });
+      allResponses.push(startRes);
+    }
+    return allResponses.join("\n");
   }
 
   usage() {
